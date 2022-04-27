e2ed792eae36da139af74002e9b1fd3060ca6d10
some_assembly_required <gabe.inbox@protonmail.com>
2022-04-27 08:56:47 -0500

    Added Weather Project

---
+++ WeatherProject/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "f016dad2056d310a3dd7e08d83908040";
+  const unit = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const weatherIcon = "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png";
+      res.write("<p>The weather is currently "+weatherDescription+".</p>")
+      res.write("<h1>The temperature in "+query+" is " + temp + " degrees Fahrenheit.</h1>");
+      res.write("<img src="+weatherIcon+">");
+    });
+  });
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server connected to port 3000.");
+});
