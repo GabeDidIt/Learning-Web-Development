95c61431b448a2f89dd48e9810c9553de3b6234d
some_assembly_required <gabe.inbox@protonmail.com>
2022-05-06 11:37:27 -0500

    Now functional and API key secure.

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,85 +1,63 @@
-//require installed node packages
 const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
 const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing");
 
-//create new express app
+require('dotenv').config();
+
 const app = express();
-
-//enable express to parse URL-encoded body i.e. info from HTML form
-app.use(express.urlencoded({extended: true}));
-
-//use static files in folder called public
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-//loads signup page on server request
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.use('/success', function(req, res){
-  res.sendFile(__dirname + '/success.html');
+client.setConfig({
+  apiKey: process.env.API_KEY,
+  server: process.env.SERVER,
 });
-app.use("/failure", function(req, res) {
-  res.sendFile(__dirname + "/failure.html")
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("dd8e819da2", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log("====== ERROR ======");
+      console.log(JSON.parse(err.response.error.text).detail);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
 });
 
 app.post("/failure", function(req, res) {
   res.redirect("/");
 });
 
-app.post("/", function(req, res) {
-
-  const firstName = req.body.fName;
-  const lastName = req.body.lName;
-  const email = req.body.email;
-
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName
-        }
-      }
-    ]
-  };
-
-
-
-  const jsonData = JSON.stringify(data);
-
-  const url = "https://us14.api.mailchimp.com/3.0/lists/dd8e819da2";
-
-  const options = {
-    method: "POST",
-    auth: "[API KEY]"
-  };
-
-  const request = https.request(url, options, function(response) {
-
-    if(response.statusCode == 200)
-      res.sendFile(__dirname+"/success.html");
-    else
-      res.sendFile(__dirname+"/failure.html");
-
-
-    response.on("data", function(data) {
-      console.log(response.statusCode);
-    });
-
-  });
-
-
-
-  request.write(jsonData);
-  request.end();
-
-});
-
-
-
 app.listen(3000, function() {
-  console.log("Server connected to port 3000.");
+  console.log("Server is running on port 3000.");
 });
